c40cddefc2fb6b831bbb8d3c7421df4f5a413ccf
Valerio Antunes de souza junior <vantunes@c1r18s12.42lisboa.com>
2021-03-30 21:50:30 +0100

    tests move

--- cub3d.c
+++ cub3d.c
@@ -1,45 +1,81 @@
 #include "mlx/mlx.h"
 #include "cub3d.h"
 
-typedef struct s_param{
+# define KEY_W			13
+# define KEY_A			0
+# define KEY_S			1
+# define KEY_D			2
+
+typedef struct s_vars
+{
+	void 	*mlx;
+	void 	*win;
+	void	*img;
+	float y;
+	float x;
+
+}	t_vars;
+
+void	draw_square(t_vars *vars, int size, int color) //funcao para formar uma quadrado de pixeis
+{
 	int		x;
 	int		y;
-	char	str[3];
-}				t_param;
 
-int				key_press(int keycode, t_param *param)
+	y = 0;
+	while (y < size)
+	{
+		x = 0;
+		while (x < size)
+		{
+			mlx_pixel_put(vars->mlx, vars->win, x + vars->x, y + vars->y, color);
+			x++;
+		}
+		y++;
+	}
+}
+
+int key_press(int keycode, t_vars *var) //move keys
 {
-	static int a = 0;
-
-	if (keycode == KEY_W)//Action when W key pressed
-		param->x++;
-	else if (keycode == KEY_S) //Action when S key pressed
-		param->x--;
-	printf("x: %d\n", param->x);
+	if (keycode == KEY_A)
+	{
+		draw_square(var, 50, 0x000000);
+		var->x-=10; //esquerda
+		draw_square(var, 50, 0x00FF0000);
+	}
+	else if (keycode == KEY_D)
+	{	
+		draw_square(var,50,0x000000);
+		var->x+=10; // direita
+		draw_square(var,50,0x00FF00);
+	}
+	else if (keycode == KEY_W)
+	{
+		draw_square(var,50,0x000000);
+		var->y-=10; // baixo
+		draw_square(var,50,0xFFFFFF);
+	}
+	else if (keycode == KEY_S)
+	{
+		draw_square(var,50,0x000000);
+		var->y+=10; //cima
+		draw_square(var,50,0x0000FF);
+	}
 	return (0);
 }
 
-int             main(void)
+int main()
 {
-    void    *mlx;
-    void    *mlx_win;
-	int		y;
-	int		x;
-	t_param		param;
+	t_vars	var;
+	float py,px; //player
 
-	y = 50;
-	mlx = mlx_init();
-    mlx_win = mlx_new_window(mlx, 1920, 1080, "Hello world!");
-	while (y < 150)
-	{
-		x = 50;
-		while (x < 150)
-		{
-			mlx_pixel_put(mlx,mlx_win,x,y,0x00FFF000);
-			x++;
-		}
-	y++;
-	}
-	mlx_hook(mlx_win, X_EVENT_KEY_PRESS, 0, &key_press, &param);
-    mlx_loop(mlx);
+	int img_width;
+	int img_height;
+
+	var.x = 500; //incial position
+	var.y = 500; // inicial position
+	var.mlx = mlx_init();
+    var.win = mlx_new_window(var.mlx, 1000, 1000, "cubd3");
+	var.img = mlx_xpm_file_to_image(var.mlx, "./textures/wall_n.xpm", &img_width, &img_height);
+	mlx_hook(var.win, 2, 1L<<0,key_press, &var);
+    mlx_loop(var.mlx);
 }
--- cub3d.h
+++ cub3d.h
@@ -7,31 +7,4 @@
 #include <string.h>
 #include <fcntl.h>
 
-#define KEY_ESC			53
-# define KEY_Q			12
-# define KEY_W			13
-# define KEY_E			14
-# define KEY_R			15
-# define KEY_A			0
-# define KEY_S			1
-# define KEY_D			2
-#define X_EVENT_KEY_PRESS		2
-#define X_EVENT_KEY_release		3
-#define X_EVENT_KEY_EXIT		17
-
-typedef struct keys {
-
-	int y;
-	int x;
-
-}	t_keys;
-
-typedef struct  s_data {
-    void        *img;
-    char        *addr;
-    int         bits_per_pixel;
-    int         line_length;
-    int         endian;
-}
-               t_data;
 #endif
