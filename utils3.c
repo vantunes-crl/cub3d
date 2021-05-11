95501ac1a7f987c3c08b2b84e391db7580ab0f9c
Valerio Antunes de souza junior <vantunes@c1r18s13.42lisboa.com>
2021-05-11 11:44:47 +0100

    pronto

--- utils3.c
+++ utils3.c
@@ -1,33 +1,5 @@
 #include "cub3d.h"
 
-int	ft_strlen(const char *str)
-{
-	int	i;
-
-	i = 0;
-	while (str[i] != '\0')
-		i++;
-	return (i);
-}
-
-char	*ft_strdup(const char *s1)
-{
-	char	*s1_cpy;
-	int		i;
-
-	i = 0;
-	s1_cpy = (char *)malloc((ft_strlen(s1) + 1) * sizeof(char));
-	if (s1_cpy == 0)
-		return (0);
-	while (s1[i])
-	{
-		s1_cpy[i] = s1[i];
-		i++;
-	}
-	s1_cpy[i] = '\0';
-	return (s1_cpy);
-}
-
 void	cell_or_floor(t_game *game, int type)
 {	
 	int	r;
@@ -63,16 +35,16 @@ int	ismap(char c)
 	return (0);
 }
 
-void free_array(char *array)
+void	free_array(char *array)
 {
 	if (!array)
 		return ;
 	free(array);
 }
 
-void free_matriz(char **matriz)
+void	free_matriz(char **matriz)
 {
-	int i;
+	int	i;
 
 	i = 0;
 	while (matriz)
---
+++ utils4.c
@@ -0,0 +1,29 @@
+#include "cub3d.h"
+
+int	ft_strlen(const char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+		i++;
+	return (i);
+}
+
+char	*ft_strdup(const char *s1)
+{
+	char	*s1_cpy;
+	int		i;
+
+	i = 0;
+	s1_cpy = (char *)malloc((ft_strlen(s1) + 1) * sizeof(char));
+	if (s1_cpy == 0)
+		return (0);
+	while (s1[i])
+	{
+		s1_cpy[i] = s1[i];
+		i++;
+	}
+	s1_cpy[i] = '\0';
+	return (s1_cpy);
+}
